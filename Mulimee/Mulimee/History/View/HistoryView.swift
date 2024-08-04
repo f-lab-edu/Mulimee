2e73be7b3ba8295408749dfa8989463d751676b4
gaeng2y <gaeng2y@gmail.com>
2024-08-04 23:05:28 +0900

    #35 MainView TabView로 수정
    - Drink 관련 서비스 파일들 Drink 폴더로 이동
    - MainView TabView 적용
    - 탭 바뀔때마다 애니메이션 새로 적용되어 생긴 이슈 수정

---
+++ Mulimee/Mulimee/History/View/HistoryView.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryView.swift
+//  Mulimee
+//
+//  Created by Kyeongmo Yang on 8/4/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        ZStack {
+            Color(.fogMist)
+                .ignoresSafeArea(edges: [.top])
+            
+            Text("History")
+        }
+    }
+}
+
+#Preview {
+    HistoryView()
+}
